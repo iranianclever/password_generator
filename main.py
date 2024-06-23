ab3d925159725d7a16d2a502135bc619fdd07d9e
iranianclever <iranianclever@gmail.com>
2024-06-23 14:06:54 +0330

    working on md5 password

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ class PasswordManager:
     def __init__(self):
         '''Initialize'''
         self.password = ''
-        self.charactors = '1234567890abcdefghijklmnopqrsvutwxyz'
+        self.characters = '1234567890abcdefghijklmnopqrsvutwxyz!@#$%^&*'
 
     def create_password(self, range_length):
         '''Create password simple'''
         self.password = ''
         for i in range(range_length):
-            num = random.randint(0, (len(self.charactors) - 1))
-            self.password += self.charactors[num]
+            num = random.randint(0, (len(self.characters) - 1))
+            self.password += self.characters[num]
         return self.password
 
     def write_password(self):
@@ -36,14 +36,21 @@ if __name__ == '__main__':
     password = ''
     while (True):
         try:
-            # Get number of charactors
-            num = int(input('How many charactors do you want for password: '))
-            # Check count of number
-            if num < 8:
-                raise Exception('Password must be at least 8 charactors')
-            # Create password
-            password = pm.create_password(num)
-            print(password)
+            print('Enter your sign to choose the type of password creation. (n) -> normal password, (m) -> password with md5, (e) -> exit.')
+            sign = input('What\'s your sign: ')
+            if sign == 'n':
+                # Get number of characters
+                num = int(input('How many characters do you want for password: '))
+                # Check count of number
+                if num < 8:
+                    raise Exception('Password must be at least 8 characters')
+                # Create password
+                password = pm.create_password(num)
+                print(password)
+            elif sign == 'm':
+                pass
+            elif sign == 'e':
+                break
             continue
         except ValueError:
             print('Please enter a number')
@@ -52,4 +59,4 @@ if __name__ == '__main__':
             print(e)
             continue
 
-    print(password)
+    print('Bye')
